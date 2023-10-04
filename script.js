30c5d710798ca33bae49ad20178b25260c50d141
Ramkrishna Roy <ramkrishnaroy324@gmail.com>
2023-10-04 20:09:25 +0530

    final commit...
    Signed-off-by: Ramkrishna Roy <ramkrishnaroy324@gmail.com>

---
+++ script.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const type = document.querySelector('.type-text');
+
+let listTexts = ['Ramkrishna','Developer', 'Web developer', 'Java Developer'];
+
+let index = -1;
+let current = 0;
+
+function typeTextFunc() {
+    if (current === listTexts.length) {
+        current = 0;
+    }
+    let typeText = setInterval(() => {
+        if (index == listTexts[current].length - 1) {
+            clearInterval(typeText);
+            clearTextFunc();
+        } else {
+            index++;
+            type.textContent += listTexts[current][index];
+        }
+    }, 150);
+};
+
+function clearTextFunc() {
+    let clearText = setInterval(() => {
+        if (type.textContent.length == 0) {
+            clearInterval(clearText);
+            index = -1;
+            current++
+            typeTextFunc();
+        } else {
+            type.textContent = type.textContent.slice(0, -1)
+        }
+    }, 150);
+};
+
+typeTextFunc();
